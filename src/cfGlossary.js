68accf3af875ae2185eac08173614489b5b73c3a
Mik Freedman <pair+mik@pivotal.io>
2018-07-04 18:16:18 -0700

    use async handler for context menu

--- src/cfGlossary.js
+++ src/cfGlossary.js
@@ -2,7 +2,21 @@ window.Glossaries = window.Glossaries || {};
 
 (function(CFGlossary) {
   function cfGlossary(chrome) {
+    function getword(info,tab) {
+      if (info.menuItemId == "cf-glossary") {
+        chrome.tabs.create({
+          url: "https://cf-glossary.cfapps.io/#" + info.selectionText
+        });
+      }
+    }
 
+    chrome.contextMenus.create({
+      id: 'cf-glossary',
+      title: "Lookup %s in CF Glossary",
+      contexts:["selection"]
+    });
+
+    chrome.contextMenus.onClicked.addListener(getword);
   }
 
   Glossaries.CFGlossary = cfGlossary;
