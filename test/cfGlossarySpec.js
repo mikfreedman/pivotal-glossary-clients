f623b7d28fca5fb756e174f5525931cf5d4e6586
Mik Freedman <pair+mik@pivotal.io>
2018-07-04 18:25:09 -0700

    add a test

--- test/cfGlossarySpec.js
+++ test/cfGlossarySpec.js
@@ -4,11 +4,30 @@ describe('Glossaries.CFGlossary', function () {
   var items = {};
 
   beforeEach(function() {
+    chrome = {
+      tabs: {
+        create: function() {
+        }
+      },
+      contextMenus: {
+        create: function() {
+        },
+        onClicked: {
+          addListener: function() {
+          }
+        }
+      }
+    }
+
+    spyOn(chrome.tabs, "create");
+    spyOn(chrome.contextMenus, "create");
+    spyOn(chrome.contextMenus.onClicked, "addListener");
   });
 
-  describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-      expect(true).toBe(true);
+  describe("CF Glossary Addon", function() {
+    it("adds the listener", function() {
+      cfGlossary = Glossaries.CFGlossary(chrome);
+      expect(chrome.contextMenus.create).toHaveBeenCalled();
     });
   });
 });
