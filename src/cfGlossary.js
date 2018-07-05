8add9fa7537419b2689257cb203552013bb7c498
Mik Freedman <pair+mik@pivotal.io>
2018-07-04 18:31:48 -0700

    slugify text

--- src/cfGlossary.js
+++ src/cfGlossary.js
@@ -2,10 +2,20 @@ window.Glossaries = window.Glossaries || {};
 
 (function(CFGlossary) {
   function cfGlossary(chrome) {
+    function slugify(text)
+    {
+      return text.toString().toLowerCase()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
+    }
+
     function getword(info,tab) {
       if (info.menuItemId == "cf-glossary") {
         chrome.tabs.create({
-          url: "https://cf-glossary.cfapps.io/#" + info.selectionText
+          url: "https://cf-glossary.cfapps.io/#" + slugify(info.selectionText)
         });
       }
     }
