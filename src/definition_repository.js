1b4e1715499f0a11c765810c10f0c362a2e5888d
Mik Freedman <pair+mik@pivotal.io>
2018-07-07 11:28:35 -0700

    WIP

--- src/definition_repository.js
+++ src/definition_repository.js
@@ -3,9 +3,29 @@ import {Definition} from './definition';
 export class DefinitionRepository {
   constructor(baseURL = "https://cf-glossary.cfapps.io") {
     this.baseURL = baseURL;
+
+    this.getData(function(response) {
+      this.definitions = _.map(Object.entries(response), function(def) {
+        return new Definition(baseURL, def);
+      });
+    });
   }
 
   find(searchText) {
     return new Definition(this.baseURL, { headword: searchText });
+  }
+
+  getData() {
+    http.get(this.baseURL + "/words.json", function(res) {
+      var body = '';
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+
+      res.on('end', function() {
+        var response = JSON.parse(body);
+        callback(response);
+      });
+    });
   }
 }
