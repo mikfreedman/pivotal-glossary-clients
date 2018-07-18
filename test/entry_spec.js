dd95df1c41cc857e60295c404805c0c4110a5083
Mik Freedman <pair+mik@pivotal.io>
2018-07-18 17:27:10 -0400

    whitespace fixes, remove extraneous describe for entry spec

--- test/entry_spec.js
+++ test/entry_spec.js
@@ -1,29 +1,27 @@
 import {Entry} from '../src/entry';
 
 describe('Entry', function () {
-  var entry;
+    var entry;
 
-  describe("matches", function() {
-    it("returns true when the slug version matches", function() {
-      entry = new Entry("http://example.com", { headword: "do^wn %trodden" });
-      expect(entry.matches("down-trodden")).toBe(true);
-    });
-    it("returns false when the slugged version does not match", function() {
-      entry = new Entry("http://example.com", { headword: "felix" });
-      expect(entry.matches("down-trodden")).toBe(false);
+    describe("matches", function () {
+        it("returns true when the slug version matches", function () {
+            entry = new Entry("http://example.com", {headword: "do^wn %trodden"});
+            expect(entry.matches("down-trodden")).toBe(true);
+        });
+        it("returns false when the slugged version does not match", function () {
+            entry = new Entry("http://example.com", {headword: "felix"});
+            expect(entry.matches("down-trodden")).toBe(false);
 
-      entry = new Entry("http://example.com", { headword: "felix cat" });
-      expect(entry.matches("felixcat")).toBe(true);
+            entry = new Entry("http://example.com", {headword: "felix cat"});
+            expect(entry.matches("felixcat")).toBe(true);
+        });
     });
-  });
 
-  describe("Entry ", function() {
-    describe("URL", function() {
-      it("returns the URL", function() {
-        entry = new Entry("http://example.com", { headword: "do^wn %trodden" });
-        expect(entry.url).toEqual("http://example.com/#downtrodden");
-      });
+    describe("URL", function () {
+        it("returns the URL", function () {
+            entry = new Entry("http://example.com", {headword: "do^wn %trodden"});
+            expect(entry.url).toEqual("http://example.com/#downtrodden");
+        });
     });
-  });
 });
 
