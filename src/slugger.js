c502a13f8430fd4c56c44c36bf263836e95955b9
Mik Freedman <pair+mik@pivotal.io>
2018-07-18 17:52:43 -0400

    Slugger should not break when not passed a value

--- src/slugger.js
+++ src/slugger.js
@@ -1,6 +1,9 @@
 export class Slugger {
   static slug(str)
   {
+    if(!str)
+      return;
+
     return str.toString().toLowerCase()
       .replace(/ /g, '-')
       .replace(/\W/g, '');
--- test/slugger_spec.js
+++ test/slugger_spec.js
@@ -1,18 +1,26 @@
 import {Slugger} from '../src/slugger';
 
 describe('Slugger', function () {
-  describe("Slugification", function() {
-    it("removes non word characters", function() {
-      expect(Slugger.slug("down$trodden")).toEqual("downtrodden");
-    });
+    describe("#slug", function () {
+        describe("when passed a value", function () {
+            it("removes non word characters", function () {
+                expect(Slugger.slug("down$trodden")).toEqual("downtrodden");
+            });
 
-    it("removes whitespace", function() {
-      expect(Slugger.slug("down  trodden")).toEqual("downtrodden");
-    });
+            it("removes whitespace", function () {
+                expect(Slugger.slug("down  trodden")).toEqual("downtrodden");
+            });
 
-    it("replaces whitespace and removes non word characters", function() {
-      expect(Slugger.slug("do^wn %trodden")).toEqual("downtrodden");
+            it("replaces whitespace and removes non word characters", function () {
+                expect(Slugger.slug("do^wn %trodden")).toEqual("downtrodden");
+            });
+        });
+
+        describe("when not passed a value", function() {
+           it("returns undefined", function() {
+              expect(Slugger.slug()).toBeUndefined();
+           });
+        });
     });
-  });
 });
 
