95cd6069b97dd565336fe7a49ba1688766dd9874
Mik Freedman <pair+mik@pivotal.io>
2018-07-20 09:35:32 -0400

    whitespace

--- src/context_menu/context_menu_search.js
+++ src/context_menu/context_menu_search.js
@@ -1,31 +1,30 @@
 export class ContextMenuSearch {
-  constructor(browser, displayEntry) {
-    this.browser = browser;
-    this.displayEntry = displayEntry;
-
-    this.browser.contextMenus.create({
-      id: 'pivotal-glossary',
-      title: "Lookup \"%s\" in Pivotal Glossary",
-      contexts:["selection"]
-    });
-
-    this.browser.contextMenus.onClicked.addListener(this.getWord.bind(this));
-  }
-
-  getWord(info) {
-    if (info.menuItemId == "pivotal-glossary") {
-      if (this.injected) {
-        this.displayEntry(info.selectionText);
-      }
-      else {
-        this.browser.tabs.executeScript({
-          file: 'context_menu_content_script.js'
-        }, () => {
-          this.displayEntry(info.selectionText);
-          this.injected = true;
+    constructor(browser, displayEntry) {
+        this.browser = browser;
+        this.displayEntry = displayEntry;
+        this.browser.contextMenus.create({
+            id: 'pivotal-glossary',
+            title: "Lookup \"%s\" in Pivotal Glossary",
+            contexts: ["selection"]
         });
-      }
+
+        this.browser.contextMenus.onClicked.addListener(this.getWord.bind(this));
     }
-  }
+
+    getWord(info) {
+        if (info.menuItemId == "pivotal-glossary") {
+            if (this.injected) {
+                this.displayEntry(info.selectionText);
+            }
+            else {
+                this.browser.tabs.executeScript({
+                    file: 'context_menu_content_script.js'
+                }, () => {
+                    this.displayEntry(info.selectionText);
+                    this.injected = true;
+                });
+            }
+        }
+    }
 }
 
