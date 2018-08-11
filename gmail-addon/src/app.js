ff9adff0bfa9ef0915af8f011b9dda7e228e1ef1
Mik Freedman <pair+mfreedman@pivotal.io>
2018-08-11 16:34:04 -0400

    add see also links

--- gmail-addon/src/app.js
+++ gmail-addon/src/app.js
@@ -16,15 +16,34 @@ export function buildEntryCard(entry) {
 
     let header = CardService.newCardHeader()
         .setTitle(entry.headword);
-    if(entry.expansion)
+    if (entry.expansion)
         header.setSubtitle(entry.expansion);
 
     card.setHeader(header);
 
-    if(entry.definition) {
+    if (entry.definition) {
         card.addSection(CardService.newCardSection()
             .setHeader('Definition')
             .addWidget(CardService.newTextParagraph().setText(entry.definition.toString())));
+    }
+
+    if (entry.see_also) {
+        var section = CardService.newCardSection()
+            .setHeader('See Also');
+
+        var buttonSet = CardService.newButtonSet();
+
+        entry.see_also.forEach(seeAlso => {
+            buttonSet.addButton(CardService.newTextButton()
+                .setText(seeAlso.headword)
+                .setOpenLink(CardService.newOpenLink()
+                    .setUrl(seeAlso.url)
+                    .setOpenAs(CardService.OpenAs.OVERLAY)));
+        });
+
+        section.addWidget(buttonSet);
+
+        card.addSection(section);
     }
 
     return card.build();
